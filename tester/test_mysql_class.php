03f80bf85f5871c2a65b15c3ac28e34231914e4a
Wan, Pei-Zhi <38753749+BaronWan@users.noreply.github.com>
2019-01-10 08:33:02 +0800

    Update test_mysql_class.php
    測試範本

--- tester/test_mysql_class.php
+++ tester/test_mysql_class.php
@@ -1,14 +1,12 @@
 <?php
 require_once "custom_MySQL_php_Class/database.class.php";
 
-$db = new Database('smallchat_db');
+$db = new Database('myDBName');
 $db->_action('select');
 
-// $db->table('im_user')->fields(array('uid','nickname','password','phone','email'))->order('uid')->desc()->limit(20)->_memory(True);
-
-$db->table('im_session_user')->fields(array('su.sessionid','ss.name as sessionName','u.nickname as userNickName','u.phone','u.email'))->_as('su')->_memory(True);
-$db->join('im_session')->_as('ss')->on(array('su.sessionid'=>'ss.id'))->_memory(True);
-$db->join('im_user')->_as('u')->on(array('su.uid'=>'u.uid'))->order('su.sessionid')->_memory(True);
+$db->table('tblName1')->fields(array('su.id','su.sessionName','u.nickname as userNickName','u.phone','u.email'))->_as('su')->_memory(True);
+$db->join('tblName2')->_as('ss')->on(array('su.id'=>'ss.id'))->_memory(True);
+$db->join('tblName3')->_as('u')->on(array('su.uid'=>'u.uid'))->order('su.id')->_memory(True);
 try {
 	$res = $db->execute(True);
 }catch (Exception $e){
